f5ac8dd2acd8cc56a7d6c64bff8d78e0da07fb48
Seung Eon Cha <winun1127@gmail.com>
2024-04-22 03:05:48 +0900

    Initial page

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+import streamlit as st
+
+
+st.markdown("당신의 학업 고민을 들려주세요!")
+st.markdown("학업 스트레스 모니터링 챗봇")
+st.title("고민모니")
+
+if st.button("로그인"):
+    st.switch_page("pages/Login.py")
+if st.button("회원가입"):
+    st.switch_page("pages/Signup.py")
