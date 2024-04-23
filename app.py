e2c1789598f74303ec913e3e2b350377faaf08ee
Dana You <danayouai@gmail.com>
2024-04-23 15:01:03 +0900

    웹페이지 로고 추가

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import os
 import streamlit as st
 
+st.set_page_config(
+    page_title = "고민모니",
+    page_icon = "./images/logo.png"
+)
+
 
 st.markdown("당신의 학업 고민을 들려주세요!")
 st.markdown("학업 스트레스 모니터링 챗봇")
