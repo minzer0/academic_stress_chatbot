761334ef84fa6739bc43a0efa1a508409aeab3fb
minzer0 <pineapple3434577@gmail.com>
2024-04-28 20:00:23 +0900

    Refactor chat filtering logic in backend.py

--- backend.py
+++ backend.py
@@ -1,19 +1,20 @@
 from st_supabase_connection import SupabaseConnection
 from datetime import datetime
+import streamlit as st
 import pandas as pd
+
+st_supabase_client = st.connection("supabase",type=SupabaseConnection)
 
 user_id = st.session_state["user_id"]
 user_name = st.session_state["user_metadata"]["user_name"]
-current_date = datetime.now().isoformat()
+current_date = datetime.now()
 
-current_year = int(current_date[:4])
-current_month = int(current_date[5:7])
-current_day = int(current_date[8:10])
+data = st_supabase_client.table("chat").select("*").execute()
+df = pd.DataFrame(data.data)
+df['created_at'] = pd.to_datetime(df['created_at'])
 
-# Filter the DataFrame based on the conditions
-filtered_df = df[(st_supabase_client.table("chat")['user_name'] == user_name) &
-                 (st_supabase_client.table("chat")['user_id'] == user_id) &
-                 (st_supabase_client.table("chat")['created_at'].dt.year == current_year) &
-                 (st_supabase_client.table("chat")['created_at'].dt.month == current_month) &
-                 (st_supabase_client.table("chat")['created_at'].dt.day == current_day)]
-
+filtered_df = df[(df['user_name'] == user_name) & 
+                 (df['user_id'] == user_id) &
+                 (df['created_at'].dt.year == current_date.year) &
+                 (df['created_at'].dt.month == current_date.month) &
+                 (df['created_at'].dt.day == current_date.day)]
