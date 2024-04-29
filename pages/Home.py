0e03fe4047c6bc2ba60b9801fa5bf9b2c4631088
DanaYou <danayouai@gmail.com>
2024-04-29 10:51:36 +0900

    Connect backend

--- pages/Home.py
+++ pages/Home.py
@@ -3,11 +3,15 @@ import numpy as np
 import altair as alt
 import pandas as pd
 from menu import menu
+from datetime import datetime
+from st_supabase_connection import SupabaseConnection
 
 from dummy_data import df_sorted
 from result_dictionary import stressor_icons
 from result_dictionary import symptoms_icons
 from result_dictionary import coping_icons
+
+from backend import average_score, percentile, summary
 
 ########################################################################################
 # SETUP 
@@ -28,76 +32,79 @@ if "user_metadata" not in st.session_state:
         st.switch_page("pages/Login.py")
     st.stop()
 
-st.title(f"{st.session_state['user_metadata']['user_name'][1:]}님의 학업 스트레스 지수")
+
+st_supabase_client = st.connection("supabase",type=SupabaseConnection)
+
+user_id = st.session_state["user_id"]
+user_name = st.session_state["user_metadata"]["user_name"]
 
 ########################################################################################
+st.title(f"{user_name}님의 학업 스트레스 지수")
 
-with st.container(border=True):
-    # 사용자 학업 스트레스 점수
-    user_score = 85
-    
-    # 사용자 학업 스트레스 점수와 해당 구간의 사람 수 표시
-    st.write(f"지난 번 {st.session_state['user_metadata']['user_name'][1:]}님의 점수는 **{user_score}**로, 또래 100명 중 **70**등이에요.")
-    st.write("**:blue[파란색]**: 나와 비슷한 점수(+/-5)를 가진 사람들 ")
+if average_score:
+    with st.container(border=True):        
+        # 사용자 학업 스트레스 점수와 해당 구간의 사람 수 표시
+        st.write(f"지난 번 {st.session_state['user_metadata']['user_name'][1:]}님의 점수는 **{average_score}**로, 또래 100명 중 **{percentile}**등이에요.")
+        st.write("**:blue[파란색]**: 나와 비슷한 점수(+/-5)를 가진 사람들 ")
 
-    # 데이터 생성
-    np.random.seed = 42  # 재현성을 위해 랜덤 시드 설정
-    mean = 70  # 평균
-    std = 10  # 표준편차
-    n_samples = 1000  # 샘플 수
+        # 데이터 생성
+        np.random.seed = 42  # 재현성을 위해 랜덤 시드 설정
+        n_samples = 1000  # 샘플 수
 
-    # 정규분포 데이터 생성
-    data = np.random.normal(mean, std, n_samples)
-    df = pd.DataFrame(data, columns=['score'])
+        # 정규분포 데이터 생성
+        data = np.random.normal(3.773399014778325, 0.9273521676028207, n_samples)
+        df = pd.DataFrame(data, columns=['score'])
 
-    # 히스토그램 생성
-    base_histogram = (
-        alt.Chart(df)
-        .mark_bar()
-        .encode(
-            x=alt.X("score:Q", bin=alt.Bin(extent=[0, 100], step=5)),  # 5점 간격으로 분할
-            y="count()",
-            color=alt.value("lightgray")
+        # 히스토그램 생성
+        base_histogram = (
+            alt.Chart(df)
+            .mark_bar()
+            .encode(
+                x=alt.X("score:Q", bin=alt.Bin(extent=[0.0, 5.0], step=0.5)),  # 5점 간격으로 분할
+                y="count()",
+                color=alt.value("lightgray")
+            )
         )
-    )
 
-    # 특정 영역 강조
-    highlight = (
-        alt.Chart(df[df['score'].between(user_score-5, user_score+5)])  # 점수 기준 +/-5 범위
-        .mark_bar(color='blue')  # 강조 색상 설정
-        .encode(
-            x=alt.X("score:Q", bin=alt.Bin(extent=[0, 100], step=5)),
-            y="count()",
+        # 특정 영역 강조
+        highlight = (
+            alt.Chart(df[df['score'].between(average_score-0.5, average_score+0.5)])  # 점수 기준 +/-5 범위
+            .mark_bar(color='blue')  # 강조 색상 설정
+            .encode(
+                x=alt.X("score:Q", bin=alt.Bin(extent=[0.0, 5.0], step=0.5)),
+                y="count()",
+            )
         )
-    )
 
-    # 히스토그램과 강조 영역 결합
-    final_chart = base_histogram + highlight
+        # 히스토그램과 강조 영역 결합
+        final_chart = base_histogram + highlight
 
-    # 차트 렌더링
-    st.altair_chart(final_chart, use_container_width=True)
-
+        # 차트 렌더링
+        st.altair_chart(final_chart, use_container_width=True)
 
 
-with st.container():
-    col1, col2, col3 = st.columns(3)
 
-    # 스트레스 원인
-    with col1:
-        st.subheader("스트레스 원인")
-        st.write(f"- {df_sorted.loc[1, '스트레스 원인']} {stressor_icons.get(df_sorted.loc[1, '스트레스 원인'], '👌')}")
+    with st.container():
+        col1, col2, col3 = st.columns(3)
 
-    # 스트레스 증상
-    with col2:
-        st.subheader("스트레스 증상")
-        st.write(f"- {df_sorted.loc[0, '스트레스 증상']} {symptoms_icons.get(df_sorted.loc[0, '스트레스 증상'], '👌')}")
-        st.write(f"- {df_sorted.loc[1, '스트레스 증상']} {symptoms_icons.get(df_sorted.loc[1, '스트레스 증상'], '👌')}")
-        st.write(f"- {df_sorted.loc[2, '스트레스 증상']} {symptoms_icons.get(df_sorted.loc[2, '스트레스 증상'], '👌')}")
-        
-    # 스트레스 대처 전략
-    with col3:
-        st.subheader("스트레스 대처 전략")
-        st.write(f"- {df_sorted.loc[1, '스트레스 대처 전략']} {coping_icons.get(df_sorted.loc[1, '스트레스 대처 전략'], '👌')}")
+        # 스트레스 원인
+        with col1:
+            st.subheader("스트레스 원인")
+            st.write(f"- {df_sorted.loc[1, '스트레스 원인']} {stressor_icons.get(df_sorted.loc[1, '스트레스 원인'], '👌')}")
+
+        # 스트레스 증상
+        with col2:
+            st.subheader("스트레스 증상")
+            st.write(f"- {df_sorted.loc[0, '스트레스 증상']} {symptoms_icons.get(df_sorted.loc[0, '스트레스 증상'], '👌')}")
+            st.write(f"- {df_sorted.loc[1, '스트레스 증상']} {symptoms_icons.get(df_sorted.loc[1, '스트레스 증상'], '👌')}")
+            st.write(f"- {df_sorted.loc[2, '스트레스 증상']} {symptoms_icons.get(df_sorted.loc[2, '스트레스 증상'], '👌')}")
+            
+        # 스트레스 대처 전략
+        with col3:
+            st.subheader("스트레스 대처 전략")
+            st.write(f"- {df_sorted.loc[1, '스트레스 대처 전략']} {coping_icons.get(df_sorted.loc[1, '스트레스 대처 전략'], '👌')}")
+else:
+    st.image('./images/nulldata.png')
 
 st.write("#")
 st.write("#")
