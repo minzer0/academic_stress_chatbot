5d2d0453ee826c55909693353a0be47473682e0e
minzer0 <pineapple3434577@gmail.com>
2024-04-29 14:02:04 +0900

    Refactor user_id and user_name handling in backend.py

--- pages/Result.py
+++ pages/Result.py
@@ -6,7 +6,7 @@ from result_dictionary import symptoms_icons
 from result_dictionary import coping_icons
 from menu import menu
 
-from backend import average_score, percentile, summary
+# from backend import average_score, percentile, summary
 from dummy_data import df_sorted
 
 ########################################################################################
@@ -34,6 +34,7 @@ if "user_id" not in st.session_state:
 user_id = st.session_state["user_id"]
 user_name = st.session_state["user_metadata"]["user_name"]
 
+exec(open("backend.py").read())
 
 summary_list = [sentence.strip() for sentence in summary.split('.') if sentence]
 ########################################################################################
