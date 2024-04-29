f292b0251e4eb5e0b5bb6bedea29b7dd98580061
minzer0 <pineapple3434577@gmail.com>
2024-04-29 13:47:24 +0900

    Update user login handling in Result.py

--- pages/Result.py
+++ pages/Result.py
@@ -25,6 +25,12 @@ with open("./.streamlit/style.css") as css:
 
 st_supabase_client = st.connection("supabase",type=SupabaseConnection)
 
+if "user_id" not in st.session_state:
+    st.error("로그인이 필요합니다.")
+    if st.button("로그인하러 가기"):
+        st.switch_page("pages/Login.py")
+    st.stop()
+
 user_id = st.session_state["user_id"]
 user_name = st.session_state["user_metadata"]["user_name"]
 
